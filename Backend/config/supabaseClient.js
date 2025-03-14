eab476b17c446b3b2aeb4f4ad6536638afd4f48f
Krish Singh Hura <95937258+krishsinghhura@users.noreply.github.com>
2025-03-14 23:54:49 +0530

    final

--- Backend/config/supabaseClient.js
+++ Backend/config/supabaseClient.js
@@ -1,8 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const supabaseUrl = "https://bemuwkmjydllrijcgtde.supabase.co"; // Replace with your Supabase URL
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlbXV3a21qeWRsbHJpamNndGRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0NTQ4MjEsImV4cCI6MjA1MzAzMDgyMX0.0VDj1RJlYkfw8D33ci61dCqp5lV78xuYNYGkp72QTlo"; // Replace with your Supabase Key
+const supabaseKey = process.env.SUPABASEKEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
